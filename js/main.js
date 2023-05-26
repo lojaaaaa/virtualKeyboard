a798a23721bda005d722f7c6c80544fa16643557
lojaaaaa <116946904+lojaaaaa@users.noreply.github.com>
2023-05-26 14:49:16 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,74 @@
+// объявление переменных
 const keyboard = document.querySelector('#keyboard')
 const keyboardButtons = document.querySelectorAll('.keyboard__button')
 const textArea = document.querySelector('#text')
 const caps = document.querySelector('#caps')
-let result = ''
 
-document.addEventListener('keydown', (event) => {
-    textArea.focus()
-    const buttonText = event.key.toLowerCase()
+const changeLang = document.querySelector('#change-lang')
+const changeTheme = document.querySelector('#change-theme')
+const settings = document.querySelector('#settings')
 
-    if(buttonText !== 'arrowleft' && buttonText !== 'arrowdown' && buttonText !== 'arrowright' && buttonText !== 'arrowup' && buttonText !== 'backspace'&& buttonText !== 'space' && buttonText !== 'capslock' && buttonText !== 'tab' && buttonText !== 'enter' && buttonText !== 'shift' && buttonText !== 'alt' && buttonText !== 'ctrl' && buttonText !== 'win'){
-        result += buttonText
-    }
-    
-    console.log(result)
+const panel = document.querySelector('#panel')
+const panelCross = document.querySelector('#panel-cross')
+const inputRange = document.querySelector('#input-range')
+const inputValue = document.querySelector('#input-value')
+
+let start = parseFloat(inputRange.min)
+let end = parseFloat(inputRange.max)
+let step = parseFloat(inputRange.step)
+
+
+inputValue.innerText = inputRange.value
+inputRange.addEventListener('input', (e)=>{
+    inputValue.innerText = inputRange.value
+    textArea.style.fontSize = inputRange.value +'px'
 })
 
-keyboardButtons.forEach(function(keyboardButton){
+let result = ''
 
+// событие по нажатию на клавиатуру
+document.addEventListener('keydown', (event) => {
+    textArea.focus()
+    result = textArea.value
+    // получаем клавишу
+    const buttonText = event.key.toLowerCase()
+    console.log(buttonText)
+    if(buttonText !== 'arrowleft' && buttonText !== 'arrowdown' && buttonText !== 'arrowright' && buttonText !== 'arrowup' && buttonText !== 'backspace' && buttonText !== 'space' && buttonText !== 'capslock' && buttonText !== 'tab' && buttonText !== 'enter' && buttonText !== 'shift' && buttonText !== 'alt' && buttonText !== 'control' && buttonText !== 'win'&& buttonText !== 'delete'){
+        result += event.key
+    }
+    if(buttonText === 'delete' || buttonText === 'backspace'){
+        result = ''
+    }
+
+
+})
+
+// Для каждой кнопки виртуальной клавиатуры добавляем событие по клику
+keyboardButtons.forEach(function(keyboardButton){
     keyboardButton.addEventListener('click', function(event){
-        console.log(event.target.firstElementChild)
-        let buttonText = event.target.firstElementChild.innerText.toLowerCase()
-        console.log(buttonText)
         textArea.focus()
+        result = textArea.value
+        let buttonText = event.target.firstElementChild.innerText.toLowerCase()
+
+        // получаем текст клавиши
+        if (changeLang.classList.contains('keyboard__change-lang--active')){
+            buttonText = event.target.lastElementChild.innerText.toLowerCase()
+        }
+        
+        // очистка
         if (buttonText === 'del'){
             result = ''
             console.clear()
         }
+        // удаление одного символа
         else if(buttonText === 'backspace'){
             result = result.substring(0, result.length - 1);
         }
         else if(buttonText === 'probel'){
             result += ' ' 
+        }
+        else if(buttonText === 'tab'){
+            result += '    ' 
         }
         else if(buttonText === 'enter'){
             result += '\n' 
@@ -44,10 +82,24 @@ keyboardButtons.forEach(function(keyboardButton){
             }
             result += buttonText
         }
-    
-        console.log(result)
+        
         textArea.value = result
         
     })
 })
 
+changeLang.addEventListener('click', (e)=>{
+    changeLang.classList.toggle('keyboard__change-lang--active')
+})
+settings.addEventListener('click', (e)=>{
+    settings.classList.toggle('keyboard__settings--active')
+    panel.classList.toggle('panel--active')
+})
+changeTheme.addEventListener('click', (e)=>{
+    changeTheme.classList.toggle('keyboard__change-theme--active')
+    textArea.classList.toggle('keyboard__text--active')
+})
+panelCross.addEventListener('click', e =>{
+    panel.classList.toggle('panel--active')
+    settings.classList.toggle('keyboard__settings--active')
+})
